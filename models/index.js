0fa7b18d9267432cf55e12d832fc6973163c3c0f
Maribel Montes <maribel.doris@gmail.com>
2022-02-09 01:31:59 -0600

    defined the index.js

--- models/index.js
+++ models/index.js
@@ -4,13 +4,34 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Products belongsTo Category, products belong to a single category
+Product.belongsTo(Category, {
+    foreignKey: 'category_id',
+}),
 
 // Categories have many Products
+Category.hasMany(Product, {
+    foreignKey: 'category_id',
+    onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'many_products'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'many_tags'
+});
 
 module.exports = {
   Product,
