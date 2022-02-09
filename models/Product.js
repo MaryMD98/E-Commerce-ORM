abd39d06a7d553ae12290fca2142ebeb190a0aef
Maribel Montes <maribel.doris@gmail.com>
2022-02-09 01:01:30 -0600

    defined the columns on product.js

--- models/Product.js
+++ models/Product.js
@@ -2,6 +2,7 @@
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
+const ProductTag = require('./ProductTag');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -10,50 +11,39 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
-
-  //   * `Product`
-
-  // * `id`
-  
-  //   * Integer.
-  
-  //   * Doesn't allow null values.
-  
-  //   * Set as primary key.
-  
-  //   * Uses auto increment.
-
-  // * `product_name`
-  
-  //   * String.
-  
-  //   * Doesn't allow null values.
-
-  // * `price`
-  
-  //   * Decimal.
-  
-  //   * Doesn't allow null values.
-  
-  //   * Validates that the value is a decimal.
-
-  // * `stock`
-  
-  //   * Integer.
-  
-  //   * Doesn't allow null values.
-  
-  //   * Set a default value of `10`.
-  
-  //   * Validates that the value is numeric.
-
-  // * `category_id`
-  
-  //   * Integer.
-  
-  //   * References the `Category` model's `id`.
-
-
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      product_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      price: {
+        type: DataTypes.DECIMAL(10,2),
+        allowNull: false,
+        validate: {
+            isDecimal: true
+        }
+      },
+      stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        default: 10,
+        validate: {
+            inInt: true
+        }
+      },
+      category_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'category',
+          key: 'id'
+          // unique: false
+        }
+      }
   },
   {
     sequelize,
