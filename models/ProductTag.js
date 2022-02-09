3af8e18bc74b41f3fcea24064abbf9a1733220e0
Maribel Montes <maribel.doris@gmail.com>
2022-02-09 00:41:53 -0600

    defined the columns on product tag

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,37 +1,34 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection');
 
+// create the product tag model
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
     // define columns
-    
-  //   * `ProductTag`
-
-  // * `id`
-
-  //   * Integer.
-
-  //   * Doesn't allow null values.
-
-  //   * Set as primary key.
-
-  //   * Uses auto increment.
-
-  // * `product_id`
-
-  //   * Integer.
-
-  //   * References the `Product` model's `id`.
-
-  // * `tag_id`
-
-  //   * Integer.
-
-  //   * References the `Tag` model's `id`.
-
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      product_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'product',
+          key: 'id',
+          unique: false
+        }
+      },
+      tag_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'tag',
+          key: 'id',
+          unique: false
+        }
+      }
   },
   {
     sequelize,
--- models/Tag.js
+++ models/Tag.js
@@ -1,7 +1,7 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection.js');
 
+// create tag model
 class Tag extends Model {}
 
 Tag.init(
